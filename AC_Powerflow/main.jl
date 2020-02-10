635bd9d241321a3f16621260ff7d5497f961944b
bhadzic1 <bhadzic1@etf.unsa.ba>
2020-02-10 10:23:37 +0100

    Promijenjen path

--- AC_Powerflow/main.jl
+++ AC_Powerflow/main.jl
@@ -4,6 +4,7 @@ using DataFrames
 using SparseArrays
 using LinearAlgebra
 using Random
+using HTTP
 
 include("inputs.jl")
 include("calculation.jl")
@@ -11,14 +12,15 @@ include("FullNR.jl")
 include("SplitNR.jl")
 include("TokoviSnage.jl")
 
-Path="D:/Fakultet/EUEES/Seminarski/PuniNR"
-Path1="D:/Fakultet/EUEES/Seminarski/RazdvojeniNR"
+Path="C:/" #Upisi path za ispis FullNR
+#Path1="C:/" #Upisi path za ispis FastNR
 Bazna=100
-sysBus = Matrix{Float64}(CSV.read("C:/Users/hadzi/github/SeminarskiEUEES/Data/BusData.csv"))
-sysLin = Matrix{Float64}(CSV.read("C:/Users/hadzi/github/SeminarskiEUEES/Data/LineData.csv"))
-# Tau1, U1, Brzina1=FullNR(sysBus,sysLin, Bazna )
-Tau2, U2, Brzina2=SplitNR(sysBus,sysLin, Bazna)
+
+sysBus = Matrix{Float64}(CSV.read((HTTP.get("https://raw.githubusercontent.com/bhadzic1/SeminarskiEUEES/master/Data/BusData.csv")).body))#Upisi path filea
+sysLin = Matrix{Float64}(CSV.read((HTTP.get("https://raw.githubusercontent.com/bhadzic1/SeminarskiEUEES/master/Data/LineData.csv")).body))#upisi path filea
+Tau1, U1, Brzina1=FullNR(sysBus,sysLin, Bazna )
+# Tau2, U2, Brzina2=SplitNR(sysBus,sysLin, Bazna)
 #Rez3=FastNR(sysBus,sysLin, Bazna) treba implementirati brzi razdvojeni NR, problem  B''
-# Proizvodnja1, Tok1=TokoviSnageAC(sysBus,sysLin,U1,Tau1,Bazna, Path)
-Proizvodnja2, Tok2=TokoviSnageAC(sysBus,sysLin,U2,Tau2,Bazna, Path1)
+Proizvodnja1, Tok1=TokoviSnageAC(sysBus,sysLin,U1,Tau1,Bazna, Path)
+# Proizvodnja2, Tok2=TokoviSnageAC(sysBus,sysLin,U2,Tau2,Bazna, Path1)
 end
